d7169688498ce8db5e7ef0a9f4c3fb803dad955c
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-09-22 17:44:23 +0300

    creating basic design

--- SmartSaver/Form1.Designer.cs
+++ SmartSaver/Form1.Designer.cs
@@ -28,13 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Budget");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Reports");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Goals");
+            this.mainContainer = new System.Windows.Forms.SplitContainer();
+            this.menuView = new System.Windows.Forms.TreeView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.secondaryContainer = new System.Windows.Forms.SplitContainer();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.mainContainer)).BeginInit();
+            this.mainContainer.Panel1.SuspendLayout();
+            this.mainContainer.Panel2.SuspendLayout();
+            this.mainContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.secondaryContainer)).BeginInit();
+            this.secondaryContainer.Panel1.SuspendLayout();
+            this.secondaryContainer.Panel2.SuspendLayout();
+            this.secondaryContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainContainer
+            // 
+            this.mainContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainContainer.Location = new System.Drawing.Point(0, 0);
+            this.mainContainer.Name = "mainContainer";
+            // 
+            // mainContainer.Panel1
+            // 
+            this.mainContainer.Panel1.Controls.Add(this.menuView);
+            this.mainContainer.Panel1.Controls.Add(this.toolStrip1);
+            this.mainContainer.Panel1.Controls.Add(this.menuStrip1);
+            this.mainContainer.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.mainContainer_Panel1_Paint);
+            // 
+            // mainContainer.Panel2
+            // 
+            this.mainContainer.Panel2.Controls.Add(this.secondaryContainer);
+            this.mainContainer.Size = new System.Drawing.Size(800, 450);
+            this.mainContainer.SplitterDistance = 200;
+            this.mainContainer.TabIndex = 0;
+            this.mainContainer.Text = "splitContainer1";
+            // 
+            // menuView
+            // 
+            this.menuView.Location = new System.Drawing.Point(0, 0);
+            this.menuView.Name = "menuView";
+            treeNode1.Name = "Budget";
+            treeNode1.Text = "Budget";
+            treeNode2.Name = "Reports";
+            treeNode2.Text = "Reports";
+            treeNode3.Name = "Goals";
+            treeNode3.Text = "Goals";
+            this.menuView.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            this.menuView.Size = new System.Drawing.Size(197, 481);
+            this.menuView.TabIndex = 4;
+            this.menuView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.menuView_AfterSelect);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(200, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // secondaryContainer
+            // 
+            this.secondaryContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.secondaryContainer.Location = new System.Drawing.Point(0, 0);
+            this.secondaryContainer.Name = "secondaryContainer";
+            // 
+            // secondaryContainer.Panel1
+            // 
+            this.secondaryContainer.Panel1.Controls.Add(this.dataGridView1);
+            this.secondaryContainer.Panel1.Controls.Add(this.textBox1);
+            this.secondaryContainer.Panel1.Controls.Add(this.vScrollBar1);
+            // 
+            // secondaryContainer.Panel2
+            // 
+            this.secondaryContainer.Panel2.Controls.Add(this.textBox2);
+            this.secondaryContainer.Size = new System.Drawing.Size(596, 450);
+            this.secondaryContainer.SplitterDistance = 400;
+            this.secondaryContainer.TabIndex = 0;
+            this.secondaryContainer.Text = "splitContainer2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 40);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(371, 260);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Text = "dataGridView1";
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(368, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(374, 0);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(26, 450);
+            this.vScrollBar1.TabIndex = 0;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(37, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "Info";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(156, 24);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(200, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(156, 24);
+            this.toolStripMenuItem2.Text = "toolStripMenuItem2";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(156, 24);
+            this.toolStripMenuItem3.Text = "toolStripMenuItem3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainContainer);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.mainContainer.Panel1.ResumeLayout(false);
+            this.mainContainer.Panel1.PerformLayout();
+            this.mainContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainContainer)).EndInit();
+            this.mainContainer.ResumeLayout(false);
+            this.secondaryContainer.Panel1.ResumeLayout(false);
+            this.secondaryContainer.Panel1.PerformLayout();
+            this.secondaryContainer.Panel2.ResumeLayout(false);
+            this.secondaryContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.secondaryContainer)).EndInit();
+            this.secondaryContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer mainContainer;
+        private System.Windows.Forms.SplitContainer secondaryContainer;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TreeView menuView;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
     }
 }
 
--- SmartSaver/Form1.cs
+++ SmartSaver/Form1.cs
@@ -17,5 +17,45 @@ namespace SmartSaver
             InitializeComponent();
         }
 
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void mainContainer_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void menuView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+            textBox1.Text = menuView.SelectedNode.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
