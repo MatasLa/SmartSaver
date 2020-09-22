db87658fcac96ffc75b57648aa8c5f06685146e3
unknown <gytis.becalis@gmail.com>
2020-09-22 09:59:55 +0300

    Entering and editing of incomes and expenses

--- SmartSaver/DataManager.cs
+++ SmartSaver/DataManager.cs
@@ -1,27 +1,103 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 namespace DataManager
 {
-	struct DataEntry
+	class DataEntry
 	{
-		public int id, value;
-		public string title;
+		/*Fields*/
+		private int id, amount;
+		private string title;
 
-		public DataEntry(int id, int value, string title)/*Testing purposes*/
+		/*Constructor*/
+		public DataEntry(int id, int amount, string title)
 		{
 			this.id = id;
-			this.value = value;
+			this.amount = amount;
 			this.title = title;
+		}
+
+		/*Getters and Setters*/
+		public string Title
+		{
+			get => title;
+			set => title = value;
+		}
+		public int Amount
+		{
+			get => amount;
+			set => amount = value;
+		}
+		public int ID
+		{
+			get => id; //Only setter, since we don't want to allow to modify id
 		}
 	}
 
+	/*Main data processing class*/
 	public class Data
 	{
-		private ArrayList income = new ArrayList();/*Both need a getter*/
-		private ArrayList expenses = new ArrayList();
+		/*Lists to store all user's incomes and expenses*/
+		private List<DataEntry> income = new List<DataEntry>();//Both need a getter
+		private List<DataEntry> expenses = new List<DataEntry>();
 
+		/*Methods that creates new instance of class and adds to List*/
+		public void AddIncome(int value, string title)
+		{
+			Random rnd = new Random();//Since no database, IDs randomized between 100 and 201
+			DataEntry newIncome = new DataEntry(rnd.Next(100, 201), value, title);
+			income.Add(newIncome);
+		}
+
+		public void AddExpense(int value, string title)
+		{
+			Random rnd = new Random();
+			DataEntry newExpense = new DataEntry(rnd.Next(100, 201), value, title);
+			expenses.Add(newExpense);
+		}
+		public bool EditIncomeItem(int id, int value)/*Returns true if success(item found), and false if failure*/
+		{
+			for(int i=0; i<income.Count;i++)
+			{
+				var temp = income[i];
+				if(temp.ID == id)
+				{
+					temp.Amount = value;
+					return true;
+				}
+			}
+			return false;
+		}
+		public bool EditIncomeItem(int id, string value)
+		{
+			for (int i = 0; i < income.Count; i++)
+			{
+				var temp = income[i];
+				if (temp.ID == id)
+				{
+					temp.Title = value;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public bool EditIncomeItem(int id, string value, int amount)
+		{
+			for (int i = 0; i < income.Count; i++)
+			{
+				var temp = income[i];
+				if (temp.ID == id)
+				{
+					temp.Title = value;
+					temp.Amount = amount;
+					return true;
+				}
+			}
+			return false;
+		}
 		public void WriteIncomeToFile()
 		{
 			File.WriteAllText("userIncome.json", "");
@@ -40,7 +116,7 @@ namespace DataManager
 					{
 						output = ",\n";
 					}
-					output = output + "{\n\t\"id\": \"" + data.id + "\",\n\t\"title\": \"" + data.title + "\",\n\t\"value\": \"" + data.value + "\"\n}";
+					output = output + "{\n\t\"id\": \"" + data.ID + "\",\n\t\"title\": \"" + data.Title + "\",\n\t\"value\": \"" + data.Amount + "\"\n}";
 					sw.Write(output);
 				}
 			}
@@ -64,7 +140,7 @@ namespace DataManager
 					{
 						output = ",\n";
 					}
-					output = output + "{\n\t\"id\": \"" + data.id + "\",\n\t\"title\": \"" + data.title + "\",\n\t\"value\": \"" + data.value + "\"\n}";
+					output = output + "{\n\t\"id\": \"" + data.ID + "\",\n\t\"title\": \"" + data.Title + "\",\n\t\"value\": \"" + data.Amount + "\"\n}";
 					sw.Write(output);
 				}
 			}
@@ -78,13 +154,12 @@ namespace DataManager
 		//Only for testing purposes, DELETE AFTER DONE TESTING!
 		/*public void test()    
 		{
-			DataEntry entry = new DataEntry(57, 200, "Internetas");
-			DataEntry entry1 = new DataEntry(87, 150, "Kuras");
-			income.Add(entry);
-			income.Add(entry1);
-			WriteIncomeToFile();
+			Data data = new Data();
+            data.AddIncome(600, "Salary");
+            data.AddIncome(150, "Stocks");
+            data.WriteIncomeToFile();
 		}*/
-		
+
 	}
 
 
