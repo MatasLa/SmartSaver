619a92a6880c8a7619f8f3276096e48311918b2e
Deividas Kučinskas <deividask.lt@gmail.com>
2020-09-25 17:58:23 +0300

    Added date funcionality

--- SmartSaver/DataManager.cs
+++ SmartSaver/DataManager.cs
@@ -13,11 +13,12 @@ namespace DataManager
 	{
 
 		/*Constructor*/
-		public DataEntry(int id, double amount, string title)
+		public DataEntry(int id, double amount, string title, DateTime date)
 		{
 			ID = id;
 			Amount = amount;
 			Title = title;
+			Date = date;
 		}
 
 		public DataEntry()
@@ -25,6 +26,7 @@ namespace DataManager
 			ID = 0;
 			Amount = 0;
 			Title = "unnamed";
+			Date = DateTime.Now;
 
 		}
 
@@ -43,6 +45,11 @@ namespace DataManager
 		{
 			get; //Only setter, since we don't want to allow to modify id
 		}
+        public DateTime Date
+		{
+			get;
+			set;
+		}
 	}
 
 	/*Main data processing class*/
@@ -53,17 +60,17 @@ namespace DataManager
         public List<DataEntry> Expenses { get; } = new List<DataEntry>();
 
 		/*Methods that creates new instance of class and adds to List*/
-		public void AddIncome(double value, string title)
+		public void AddIncome(double value, string title, DateTime date)
 		{
 			Random rnd = new Random();//Since no database, IDs randomized between 100 and 201
-			DataEntry newIncome = new DataEntry(rnd.Next(100, 201), value, title);
+			DataEntry newIncome = new DataEntry(rnd.Next(100, 201), value, title, date);
 			Income.Add(newIncome);
 		}
 
-		public void AddExpense(double value, string title)
+		public void AddExpense(double value, string title, DateTime date)
 		{
 			Random rnd = new Random();
-			DataEntry newExpense = new DataEntry(rnd.Next(100, 201), value, title);
+			DataEntry newExpense = new DataEntry(rnd.Next(100, 201), value, title, date);
 			Expenses.Add(newExpense);
 		}
 
@@ -81,14 +88,23 @@ namespace DataManager
 			temp.Title = value;
 			return true;
 		}
+		
+		public bool EditIncomeItem(int id, DateTime date)
+        {
+			var temp = Income.FirstOrDefault(x => x.ID == id);
+			temp.Date = date;
+			return true;
+		}
 
-		public bool EditIncomeItem(int id, string value, double amount)
+		public bool EditIncomeItem(int id, string value, double amount, DateTime date)
 		{
 			var temp = Income.FirstOrDefault(x => x.ID == id);
 			temp.Title = value;
 			temp.Amount = amount;
+			temp.Date = date;
 			return true;
 		}
+		
 		public bool EditExpensesItem(int id, double value)/*Returns true if success(item found), and false if failure*/
 		{
 			var temp = Expenses.FirstOrDefault(x => x.ID == id);
@@ -101,12 +117,19 @@ namespace DataManager
 			temp.Title = value;
 			return true;
 		}
+		public bool EditExpensesItem(int id, DateTime date)
+		{
+			var temp = Expenses.FirstOrDefault(x => x.ID == id);
+			temp.Date = date;
+			return true;
+		}
 
-		public bool EditExpensesItem(int id, string value, double amount)
+		public bool EditExpensesItem(int id, string value, double amount, DateTime date)
 		{
 			var temp = Expenses.FirstOrDefault(x => x.ID == id);
 			temp.Title = value;
 			temp.Amount = amount;
+			temp.Date = date; 
 			return true;
 		}
 
