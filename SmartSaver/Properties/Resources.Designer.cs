ce976c4063e711b32e238e3e6a493168f0347c9a
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-09-25 18:55:41 +0300

    merged with dev and deividas branch, will test if everything works

--- SmartSaver/Properties/Resources.Designer.cs
+++ SmartSaver/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TimeManager.Properties {
+namespace SmartSaver.Properties {
     using System;
     
     
