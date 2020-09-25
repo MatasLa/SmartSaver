8293530dbd16baf4fa2dcabafb1140715e0c7c4b
unknown <gytis.becalis@gmail.com>
2020-09-25 14:18:39 +0300

    Added convertion of local data to DataTable

---
+++ SmartSaver/DataTableConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using DataManager;
+
+namespace SmartSaver
+{
+    class DataTableConverter
+    {
+        private Data Data { get; }
+
+        public DataTable incomeTable()
+        {
+            var dt = new DataTable();
+            var dc1 = new DataColumn("ID", typeof(int));
+            var dc2 = new DataColumn("Title", typeof(string));
+            var dc3 = new DataColumn("Amount", typeof(double));
+            dt.Columns.Add(dc1);
+            dt.Columns.Add(dc2);
+            dt.Columns.Add(dc3);
+
+            var temp = Data.Income;
+            foreach(DataEntry data in temp)
+            {
+                dt.Rows.Add(data.ID, data.Title, data.Amount);
+            }
+            return dt;
+        }
+
+        public DataTable expensesTable()
+        {
+            var dt = new DataTable();
+            var dc1 = new DataColumn("ID", typeof(int));
+            var dc2 = new DataColumn("Title", typeof(string));
+            var dc3 = new DataColumn("Amount", typeof(double));
+            dt.Columns.Add(dc1);
+            dt.Columns.Add(dc2);
+            dt.Columns.Add(dc3);
+
+            var temp = Data.Expenses;
+            foreach (DataEntry data in temp)
+            {
+                dt.Rows.Add(data.ID, data.Title, data.Amount);
+            }
+            return dt;
+        }
+
+
+    }
+         
+}
