936a5b0924c4c33331dfff06276ae402a22da653
Gytis Bečalis <gytis.becalis@gmail.com>
2020-09-26 12:48:51 +0300

    Added field 'isMonthly' for DataEntry; filtering and creating Lists by various criteria; method that returns custom DataTable according to filtered List

---
+++ SmartSaver/DataFilter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataManager
+{
+	/*Requires datamanager object, methods filter data by provided criteria*/
+    class DataFilter
+    {
+        private Data data;
+        public DataFilter(Data data)
+        {
+            this.data = data;
+        }
+
+		public List<DataEntry> GetIncomeHigherThan(double amount)
+		{
+			List<DataEntry> temp = data.Income.Where(x => x.Amount >= amount).ToList();
+			return temp;
+		}
+
+		public List<DataEntry> GetExpensesHigherThan(double amount)
+		{
+			List<DataEntry> temp = data.Income.Where(x => x.Amount >= amount).ToList();
+			return temp;
+		}
+
+		public List<DataEntry> GetIncomeByDate(DateTime dateTime)
+		{
+			List<DataEntry> temp = data.Income.Where(x => (x.Date.Year == dateTime.Year) && (x.Date.Month == dateTime.Month)).ToList();
+			return temp;
+		}
+
+		public List<DataEntry> GetExpensesByDate(DateTime dateTime)
+		{
+			List<DataEntry> temp = data.Expenses.Where(x => (x.Date.Year == dateTime.Year) && (x.Date.Month == dateTime.Month)).ToList();
+			return temp;
+		}
+
+		public List<DataEntry> GetReccuringIncome()
+		{
+			List<DataEntry> temp = data.Income.Where(x => x.IsMonthly).ToList();
+			return temp;
+		}
+
+		public List<DataEntry> GetReccuringExpenses()
+		{
+			List<DataEntry> temp = data.Expenses.Where(x => x.IsMonthly).ToList();
+			return temp;
+		}
+	}
+}
--- SmartSaver/DataTableConverter.cs
+++ SmartSaver/DataTableConverter.cs
@@ -13,32 +13,51 @@ namespace DataManager
             this.data = data;
         }
 
-        public DataTable incomeTable()
+        public DataTable IncomeTable()//All entries
         {
             var dt = new DataTable();
             dt.Columns.Add("ID", typeof(int));
             dt.Columns.Add("Title", typeof(string));
             dt.Columns.Add("Amount", typeof(double));
             dt.Columns.Add("Date", typeof(DateTime));
+            dt.Columns.Add("Recurring", typeof(bool));
 
             foreach(DataEntry data in data.Income)
             {
-                dt.Rows.Add(data.ID, data.Title, data.Amount, data.Date);
+                dt.Rows.Add(data.ID, data.Title, data.Amount, data.Date, data.IsMonthly);
             }
             return dt;
         }
 
-        public DataTable expensesTable()
+        public DataTable ExpensesTable()//All entries
         {
             var dt = new DataTable();
             dt.Columns.Add("ID", typeof(int));
             dt.Columns.Add("Title", typeof(string));
             dt.Columns.Add("Amount", typeof(double));
             dt.Columns.Add("Date", typeof(DateTime));
+            dt.Columns.Add("Recurring", typeof(bool));
 
             foreach (DataEntry data in data.Expenses)
             {
-                dt.Rows.Add(data.ID, data.Title, data.Amount, data.Date);
+                dt.Rows.Add(data.ID, data.Title, data.Amount, data.Date, data.IsMonthly);
+            }
+            return dt;
+        }
+
+        /*Takes list from DataFilter and creates income or expenses DataTable, used to get DataTable of filtered List*/
+        public DataTable CustomTable(List<DataEntry> customList)
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Title", typeof(string));
+            dt.Columns.Add("Amount", typeof(double));
+            dt.Columns.Add("Date", typeof(DateTime));
+            dt.Columns.Add("Recurring", typeof(bool));
+
+            foreach (DataEntry data in customList)
+            {
+                dt.Rows.Add(data.ID, data.Title, data.Amount, data.Date, data.IsMonthly);
             }
             return dt;
         }
