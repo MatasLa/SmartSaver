794daff4b26dfd7a7ded5a43379b9899ac9f2103
Gytis Bečalis <gytis.becalis@gmail.com>
2020-09-30 21:15:37 +0300

    structure of login

--- SmartSaver/forms/FormLogIn.cs
+++ SmartSaver/forms/FormLogIn.cs
@@ -15,5 +15,30 @@ namespace Forms
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void FormLogIn_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
