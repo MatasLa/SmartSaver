b2bf42df9c32cb672c1bfd5552d65eef78ae69a9
Deividas Kučinskas <deividask.lt@gmail.com>
2020-10-02 18:55:39 +0300

    Basic calculation logic skeleton

--- SmartSaver/DataCalculations.cs
+++ SmartSaver/DataCalculations.cs
@@ -1,4 +1,6 @@
-﻿namespace DataManager
+﻿using Forms;
+
+namespace DataManager
 {
     class DataCalculations
     {
@@ -32,6 +34,38 @@
                 return false;
             }
         }
+        //Preferably dont use code below this comment yet, names and functionality might change, this is a rough idea
+        public bool CheckGoal()
+        {
+            if (IsBalancePositive())
+            {
+                Goal goal = new Goal();
+                if((CheckBalance() - goal.price) >= 0)
+                {
+                    return true;    //can already buy this month
+                }
+                else
+                {
+                    return SavingMoney(); //not in debt, but needs saving
+                }
+            }
+            else
+            {
+                return false;   //user already in debt without adding goal expenses
+            }
+        }
+        public bool SavingMoney()
+        {
+            var savedAmount = 0;
+            Goal goal = new Goal();
+            var neededAmount = (goal.price - CheckBalance());
+            while ((neededAmount - savedAmount) > 0) //while(can't afford goal)
+            {   //todo: implement saving, export the categories/things needed to save on
+                
+                //saving happening (savedAmount going up)
 
+            }
+            return true; // after having saved enough
+        }
     }
 }
