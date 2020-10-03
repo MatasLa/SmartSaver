ae674181add1a2e1a60cd591d099a76a23580046
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-10-03 08:28:01 +0300

    made swapping between forms more efficient, cleaned up code

--- SmartSaver/forms/FormMain.cs
+++ SmartSaver/forms/FormMain.cs
@@ -18,6 +18,8 @@ namespace Forms
     public partial class FormMain : Form
     {
         public Handler DataHandler { get; }
+        //we need to store the previous form to close it to open the new form
+        private Form activeForm = null;
 
         public FormMain(Handler dataHandler)
         {
@@ -32,6 +34,75 @@ namespace Forms
             panelReportsSubMenu.Visible = false;
         }
 
+        private void ShowSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+        private void OpenChildForm(Form childForm)
+        {
+            if (activeForm != null)
+            {
+                if (IsSameFormAlreadyOpen(childForm))
+                {
+                    if (activeForm is FinanceForm form)
+                    {
+                        form.EntryType = ((FinanceForm)childForm).EntryType;
+                    }
+                    return;
+                }
+                else
+                {
+                    activeForm.Close();
+                }
+            }
+
+            activeForm = childForm;
+            //it will behave like a control
+            childForm.TopLevel = false;
+            // make the form the up the whole panel
+            childForm.Dock = DockStyle.Fill;
+            //add the form to the list of the controls in the container panel
+            panelMain.Controls.Add(childForm);
+            // associate the form with the container panel
+            panelMain.Tag = childForm;
+            // to hide the logo
+            childForm.BringToFront();
+            childForm.Show();
+            
+        }
+
+        private bool IsSameFormAlreadyOpen(object form)
+        {
+            if(activeForm.GetType() == form.GetType())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void CloseChildForm()
+        {
+            if (activeForm != null)
+            {
+                activeForm.Close();
+                activeForm = null;
+            }
+        }
+
+
+        #region button event handlers
+
         private void ButtonFinances_Click(object sender, EventArgs e)
         {
             ShowSubMenu(panelFinancesSubMenu);
@@ -44,38 +115,17 @@ namespace Forms
 
         private void ButtonBudget_Click(object sender, EventArgs e)
         {
-            if (activeForm is FinanceForm form)
-            {
-                form.EntryType = EntryType.Expense;
-            }
-            else
-            {
-                OpenChildForm(new FinanceForm(DataHandler, EntryType.Expense));
-            }
+            OpenChildForm(new FinanceForm(DataHandler, EntryType.Expense));
         }
 
         private void ButtonIncome_Click(object sender, EventArgs e)
         {
-            if (activeForm is FinanceForm form)
-            {
-                form.EntryType = EntryType.Income;
-            }
-            else
-            {
-                OpenChildForm(new FinanceForm(DataHandler, EntryType.Income));
-            }
+            OpenChildForm(new FinanceForm(DataHandler, EntryType.Income));
         }
 
         private void ButtonExpenses_Click(object sender, EventArgs e)
         {
-            if (activeForm is FinanceForm form)
-            {
-                form.EntryType = EntryType.Expense;
-            }
-            else
-            {
-                OpenChildForm(new FinanceForm(DataHandler, EntryType.Expense));
-            }
+            OpenChildForm(new FinanceForm(DataHandler, EntryType.Expense));
         }
 
         private void ButtonGoals_Click(object sender, EventArgs e)
@@ -87,7 +137,6 @@ namespace Forms
         {
             OpenChildForm(new FormSpending());
         }
-
 
         private void ButtonNetWorth_Click(object sender, EventArgs e)
         {
@@ -119,67 +168,8 @@ namespace Forms
             CloseChildForm();
         }
 
+        #endregion
 
-        private void ShowSubMenu(Panel subMenu)
-        {
-            if (subMenu.Visible == false)
-            {
-                subMenu.Visible = true;
-            }
-            else
-            {
-                subMenu.Visible = false;
-            }
-        }
-
-        //we need to store the previous form to close it to open the new form
-        private Form activeForm = null;
-        private void OpenChildForm(Form childForm)
-        {
-            if (activeForm != null)
-            {
-                activeForm.Close();
-            }
-            else
-            {
-                if (!IsSameFormAlreadyOpen(childForm))
-                {
-                    activeForm = childForm;
-                    //it will behave like a control
-                    childForm.TopLevel = false;
-                    // make the form the up the whole panel
-                    childForm.Dock = DockStyle.Fill;
-                    //add the form to the list of the controls in the container panel
-                    panelMain.Controls.Add(childForm);
-                    // associate the form with the container panel
-                    panelMain.Tag = childForm;
-                    // to hide the logo
-                    childForm.BringToFront();
-                    childForm.Show();
-                }
-            }
-        }
-
-        private bool IsSameFormAlreadyOpen(Form form)
-        {
-            if(activeForm.GetType() == form.GetType())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private void CloseChildForm()
-        {
-            if (activeForm != null)
-            {
-                activeForm.Close();
-                activeForm = null;
-            }
-        }
     }
-    
+
 }
