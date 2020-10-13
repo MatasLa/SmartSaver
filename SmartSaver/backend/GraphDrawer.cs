c2e87119b36629ff686136a812622157ce3d8ce4
unknown <matas.lazdauskas@gmail.com>
2020-10-13 13:27:38 +0300

    removed debug lines

--- SmartSaver/backend/GraphDrawer.cs
+++ SmartSaver/backend/GraphDrawer.cs
@@ -22,7 +22,12 @@ namespace ePiggy.backend
             string exp = expenses.ToString(nfi);
             string chartInfo = String.Format("a:{0},{1}", inc, exp);
 
-            ImageCharts pie = new ImageCharts().cht("p").chl("Incomes|Expenses").chco("00b7ff|eb5244").chd(chartInfo).chs("400x400");
+            ImageCharts pie = new ImageCharts()
+                .cht("p")
+                .chl("Incomes|Expenses")
+                .chco("00b7ff|eb5244")
+                .chd(chartInfo)
+                .chs("400x400");
 
             string projectDirectory = System.IO.Directory.GetCurrentDirectory().Replace("\\bin\\Debug\\netcoreapp3.1", "");
 
