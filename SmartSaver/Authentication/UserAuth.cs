ec8110e22af52badbbd3c18df7bcd5ec7cb55f6f
unknown <matas.lazdauskas@gmail.com>
2020-10-20 02:49:40 +0300

    added ChangePassword and SendCode methods for password recovery

--- SmartSaver/Authentication/UserAuth.cs
+++ SmartSaver/Authentication/UserAuth.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using ePiggy.DataBase;
 using ePiggy.DataBase.Models;
 
@@ -52,6 +55,62 @@ namespace ePiggy.Authentication
                 {
                     return false;
                 }
+            }
+        }
+
+        public static bool ChangePassword(string email, string pass)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var userInfo = db.Users.Where(a => a.Email == email).FirstOrDefault();
+
+                if (userInfo != null)
+                {
+                    var salt = HashingProcessor.CreateSalt(20);
+                    var passwordHash = HashingProcessor.GenerateHash(pass, salt);
+
+                    userInfo.Password = passwordHash;
+                    userInfo.Salt = salt;
+                    db.SaveChanges();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static int SendCode(string email)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var userInfo = db.Users.Where(a => a.Email == email).FirstOrDefault();
+
+                if (userInfo != null)
+                {
+                    string from, pass, messageBody;
+                    Random rand = new Random();
+                    int randomCode = rand.Next(999999);
+                    MailMessage message = new MailMessage();
+                    from = "smartsaverrecovery@gmail.com";
+                    pass = "Smartsaver123456";
+                    messageBody = "Your password recovery code is: " + randomCode;
+                    message.To.Add(email);
+                    message.From = new MailAddress(from);
+                    message.Body = messageBody;
+                    message.Subject = "Password Recovery";
+                    SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                    smtp.EnableSsl = true;
+                    smtp.Port = 587;
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Credentials = new NetworkCredential(from, pass);
+
+                    smtp.Send(message);
+                    return randomCode;
+                }
+                else return 0;
             }
         }
 
