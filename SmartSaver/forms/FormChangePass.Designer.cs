af0e0673fd7d55429281d8b1c3176e435e5206b6
Gytis Bečalis <gytis.becalis@gmail.com>
2020-10-20 13:49:22 +0300

    FormChangePass start position - center of screen

--- SmartSaver/forms/FormChangePass.Designer.cs
+++ SmartSaver/forms/FormChangePass.Designer.cs
@@ -126,6 +126,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormChangePass";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ePiggy";
             this.ResumeLayout(false);
             this.PerformLayout();
