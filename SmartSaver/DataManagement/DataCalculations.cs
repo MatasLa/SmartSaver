dc996cd9527cfba880f7861c45a10110e2756fe1
Arnas Rimkus <arnas.rimkus@yahoo.com>
2020-10-27 13:19:19 +0200

    addressed some of the issues raised in the pull request

--- SmartSaver/DataManagement/DataCalculations.cs
+++ SmartSaver/DataManagement/DataCalculations.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using ePiggy.Utilities;
 
 namespace ePiggy.DataManagement
@@ -10,30 +9,25 @@ namespace ePiggy.DataManagement
         private readonly Data _data;
         private readonly DataFilter _dataFilter;
         private readonly DataTotalsCalculator _dataTotalsCalculator;
+
+        private SavingType _savingType;
+
+        //This might not be 1M in the future, that why we are keeping this
+        private const decimal SavingRatio = 1M;
+        private const decimal RegularSavingValue = 0.25M;
+        private const decimal MaximalSavingValue = 0.5M;
+        private const decimal MinimalSavingValue = 0.1M;
+
+        private List<EntrySuggestion> IncomeOffers { get; } = new List<EntrySuggestion>();
+        private List<EntrySuggestion> ExpensesOffers { get; } = new List<EntrySuggestion>();
         public DataCalculations(Data data, DataFilter dataFilter, DataTotalsCalculator dataTotalsCalculator)
         {
             _data = data;
             _dataFilter = dataFilter;
             _dataTotalsCalculator = dataTotalsCalculator;
+            _savingType = SavingType.Regular;
         }
 
-        private SavingType _savingType;
-        private const decimal SavingRatio = 1M;
-
-        //Temporary hardcoded local parameters (should be taken from front-end later)
-        private const decimal RegularSavingValue = 0.25M;
-        private const decimal MaximalSavingValue = 0.5M;
-        private const decimal MinimalSavingValue = 0.1M;
-
-        //Kept these ratios in case we decide to change how we calculate things 
-        //private const decimal HighRatio = 1M;
-        //private const decimal MediumRatio = 2M;
-        //private const decimal LowRatio = 3M;
-        //private const decimal UnnecessaryRatio = 4M;
-
-        // These two lists are updated but never used according to resharper
-        public List<EntrySuggestion> IncomeOffers { get; } = new List<EntrySuggestion>();
-        public List<EntrySuggestion> ExpensesOffers { get; } = new List<EntrySuggestion>();
         
 
         public static int CalculateProgress(decimal saved, decimal target)
@@ -51,13 +45,7 @@ namespace ePiggy.DataManagement
         }
 
         
-        public bool IsBalancePositive()/*Same thing in DataFilter but by Date "IsBalancePositiveByDate"*/
-        {
-            return _dataTotalsCalculator.GetBalance() >= 0;
-        }
-
-        //WIP
-        public bool GetSuggestedExpensesOffers(List<DataEntry> entryList, Goal goal, SavingType savingType, List<EntrySuggestion> offerList)
+        public bool GetSuggestedExpensesOffers(List<DataEntry> entryList, Goal goal, SavingType savingType, List<EntrySuggestion> entrySuggestions)
         {
             _savingType = savingType;
             var savedAmount = 0M;
@@ -78,15 +66,15 @@ namespace ePiggy.DataManagement
                         case SavingType.Regular:
                             amountAfterSaving = entry.Amount * SavingRatio * (i - 1) * RegularSavingValue;
                             break;
-                        default:
-                        {
-                            var maximalSaving = (MaximalSavingValue * SavingRatio * (i - 1)) >= 1;
+                        case SavingType.Maximal:
+                            var maximalSaving = MaximalSavingValue * SavingRatio * (i - 1) >= 1;
                             var temp = maximalSaving ? 1M : MaximalSavingValue;
                             amountAfterSaving = entry.Amount * temp;
                             break;
-                        }
+                        default:
+                            throw new ArgumentOutOfRangeException();
                     }
-                    AddToExpensesOfferList(entry, amountAfterSaving, offerList);
+                    AddToExpensesOfferList(entry, amountAfterSaving, entrySuggestions);
 
                     savedAmount += entry.Amount - amountAfterSaving;
                     if(goal.Price <= savedAmount)
@@ -97,15 +85,14 @@ namespace ePiggy.DataManagement
             }
             return false; //Didn't save enough
         }
-        //
 
         public bool CheckGoal(Goal goal, SavingType savingType)
         {
             _savingType = savingType;
-            if (IsBalancePositive())
+            if (_dataTotalsCalculator.GetBalance() >= 0)
             {
                 //Goal goal = new Goal();
-                return (_dataTotalsCalculator.GetBalance() - goal.Price) >= 0 || GenerallySavingMoney();
+                return _dataTotalsCalculator.GetBalance() - goal.Price >= 0 || GenerallySavingMoney();
             }
 
             return false;   //user already in debt(negative balance) without adding goal expenses
@@ -128,30 +115,6 @@ namespace ePiggy.DataManagement
         {
             return dataEntry.Importance == (int)Importance.Necessary 
                    || ImportanceBasedCalculation(dataEntry, SavingRatio * (dataEntry.Importance - 1), entryType);
-
-            //Worth keeping, if we ever want to adjust calculation ratios for balancing
-
-            //switch (dataEntry.Importance)
-            //{
-            //    case (int)Importance.Necessary:
-            //        return true; //importance of necessary - unchangable income
-
-            //    case (int)Importance.High:
-            //        return ImportanceBasedCalculation(dataEntry, SavingRatio * HighRatio, entryType);
-
-            //    case (int)Importance.Medium:
-            //        return ImportanceBasedCalculation(dataEntry, SavingRatio * MediumRatio, entryType);
-
-            //    case (int)Importance.Low:
-            //        return ImportanceBasedCalculation(dataEntry, SavingRatio * LowRatio, entryType);
-
-            //    case (int)Importance.Unnecessary:
-            //        return ImportanceBasedCalculation(dataEntry, SavingRatio * UnnecessaryRatio, entryType);
-
-            //    default:
-            //        return true;
-
-            //}
         }
 
         // ImportanceBasedCalculation always returns true, doesn't really make sense for a method to always return true
@@ -163,10 +126,10 @@ namespace ePiggy.DataManagement
                       switch (entryType)
                       {
                           case EntryType.Income:
-                              AddToIncomeOfferList(dataEntry, dataEntry.Amount * (MinimalSavingValue * importanceBasedSavingRatio));
+                              AddToIncomeOfferList(dataEntry, dataEntry.Amount * MinimalSavingValue * importanceBasedSavingRatio);
                               break;
                           case EntryType.Expense:
-                              AddToExpensesOfferList(dataEntry, dataEntry.Amount * (MinimalSavingValue * importanceBasedSavingRatio));
+                              AddToExpensesOfferList(dataEntry, dataEntry.Amount * MinimalSavingValue * importanceBasedSavingRatio);
                               break;
                           default:
                               throw new ArgumentOutOfRangeException(nameof(entryType), entryType, null);
@@ -174,7 +137,7 @@ namespace ePiggy.DataManagement
                       return true;
                   case SavingType.Maximal:
                   {
-                      var maximalSaving = (MaximalSavingValue * importanceBasedSavingRatio) >= 1;
+                      var maximalSaving = MaximalSavingValue * importanceBasedSavingRatio >= 1;
 
                       var temp = maximalSaving ? 1M : MaximalSavingValue;
 
@@ -195,10 +158,10 @@ namespace ePiggy.DataManagement
                       switch (entryType)
                       {
                           case EntryType.Income:
-                              AddToIncomeOfferList(dataEntry, dataEntry.Amount * (RegularSavingValue * importanceBasedSavingRatio));
+                              AddToIncomeOfferList(dataEntry, dataEntry.Amount * RegularSavingValue * importanceBasedSavingRatio);
                               break;
                           case EntryType.Expense:
-                              AddToExpensesOfferList(dataEntry, dataEntry.Amount * (RegularSavingValue * importanceBasedSavingRatio));
+                              AddToExpensesOfferList(dataEntry, dataEntry.Amount * RegularSavingValue * importanceBasedSavingRatio);
                               break;
                           default:
                               throw new ArgumentOutOfRangeException(nameof(entryType), entryType, null);
@@ -210,7 +173,6 @@ namespace ePiggy.DataManagement
               return true;
         }
 
-        // Methods below should be moved to another class, because of Single Responsibility principle - these are not calculations
 
         private void AddToIncomeOfferList(DataEntry entry, decimal amountAfterSaving)
         {
@@ -224,12 +186,10 @@ namespace ePiggy.DataManagement
             ExpensesOffers.Add(newExpensesOffer);
         }
 
-        private static void AddToExpensesOfferList(DataEntry entry, decimal amountAfterSaving, List<EntrySuggestion> entrySuggestions)
+        private static void AddToExpensesOfferList(DataEntry entry, decimal amountAfterSaving, ICollection<EntrySuggestion> entrySuggestions)
         {
             var newExpensesOffer = new EntrySuggestion(entry, amountAfterSaving);
             entrySuggestions.Add(newExpensesOffer);
         }
-
-        // Methods above should be moved to another class, because of Single Responsibility principle - these are not calculations
     }
 }
